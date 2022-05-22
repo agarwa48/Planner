d82733bf0ffd6ba86bd413a24e0cb193950afc5e
Vyomika <vyomikaagarwal@Vyomika-2.local>
2022-05-22 20:11:52 +0100

    ALMOST FINAL PROJECT

--- Schedule.java
+++ Schedule.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
 import java.lang.reflect.Array;
-
 import javax.swing.plaf.DimensionUIResource;
 import javax.xml.validation.Validator;
 import java.time.LocalTime;
@@ -16,7 +15,7 @@ public class schedule implements ActionListener
 
     JLabel headerLabel[];
     JTextField headField[];
-    String headString[] = {"HeadLiner event:", "Staring time:"};
+    String headString[] = {"HeadLiner event:","Duration", "Staring time:"};
 
     JLabel nameLabel[];
     JTextField eventField[];
@@ -38,11 +37,13 @@ public class schedule implements ActionListener
     
     JComboBox priorityBox;
 
-    String[] priority = {"1", "2", "3", "4", "5"};      // contents of the priority box
+    String[] priority = {"2", "3", "4", "5"};      // contents of the priority box
 
     int i;
     int j;
     int a;  
+
+    ArrayList<ArrayList<String> > tableList;
 
     // main constructor
     public schedule()
@@ -55,8 +56,8 @@ public class schedule implements ActionListener
         eventPanel = new JPanel();                               // panel for other events
         eventPanel.setLayout(new GridLayout(9,1,10,10));
 
-        headField = new JTextField[2];                  // array for the 2 textfields in the head panel
-        headerLabel = new JLabel[2];                    // array for the 2 text labels in the head panel
+        headField = new JTextField[3];                  // array for the 2 textfields in the head panel
+        headerLabel = new JLabel[3];                    // array for the 2 text labels in the head panel
 
         eventField = new JTextField[3];                 // array for the 3 textfields in the event panel
         nameLabel = new JLabel[4];                      // array for the 3 text labels in the event panel
@@ -67,11 +68,12 @@ public class schedule implements ActionListener
         finalPanel2 = new JPanel();                     // panel(1) inside the 3rd page panel
         finalPanel.setLayout(new GridLayout(4,1));           
 
+        tableList = new ArrayList<ArrayList<String> >();
 
         priorityBox = new JComboBox(priority);
 
 
-        for(j = 0; j<2; j++)                             // for loop for printing out the 2 text fields and labels in the headpanel
+        for(j = 0; j<3; j++)                             // for loop for printing out the 2 text fields and labels in the headpanel
         {
             headerLabel[j] = new JLabel(headString[j]);
             headPanel.add(headerLabel[j]);
@@ -156,16 +158,40 @@ public class schedule implements ActionListener
 
     Object[][] data =                   // initialising the 2d array outside the method to access it elsewhere
     {
-       // {"Headliner event", "3 hours", "12:00", "1"},
+        {"Headliner event", "3 hours", "12:00", "1"},
         {"event2", "2 hours", "15:00", "2"}
     };
 
+
     public void table() // table method
     {
+                //Get the components in the panel
+        Component[] componentList = finalPanel.getComponents();
+
+        //Loop through the components
+        for(Component c : componentList){
+
+            //Find the components you want to remove
+            if(c instanceof JScrollPane){
+
+                //Remove it
+                finalPanel.remove(c);
+            }
+        }
+
+        int length = tableList.size();    //get the number of elements in the table List
+        String[][] temp = new String[length][4];        // rows and cols
+        for(i = 0; i<length; i++)
+        {
+            for(j = 0; j<4; j++)
+            {
+                temp[i][j] = tableList.get(i).get(j);        // getting valies from main and small array List
+            }
+        }
 
         String[] colNames = {"Name", "Duration", "Time", "Priority"};
 
-        table = new JTable(data, colNames);
+        table = new JTable(temp, colNames);
 
         table.setLayout(new GridLayout());
 
@@ -173,37 +199,48 @@ public class schedule implements ActionListener
         JScrollPane scrollPane2 = new JScrollPane(table);
         //scrollpane2++;
 
+        
         finalPanel.add(scrollPane);
-        finalPanel.add(scrollPane2);
         finalPanel.remove(scrollPane);
+        finalPanel.add(scrollPane2);
+        
 
+        finalPanel.revalidate();
     }
 
 
-          // making a new method for action listener outside the constructor
+    // making a new method for action listener outside the constructor
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        System.out.println(data.length);
         Object[][] newData = new Object[data.length+1][4];  // creates a copy of the old array but bigger
-        System.arraycopy(data,0 , newData, 1, data.length);  // DEST POS CHANGES 
+        System.arraycopy(data,0 , newData, 1, data.length);  // still copies the same items before the new row is made
         System.out.println(String.valueOf(newData));
+        System.out.println(Arrays.deepToString(newData));
 
         if(e.getSource()==submit)
         {
-            newData[data.length][0]= headField[0].getText();
-            newData[data.length][1]= headField[1].getText();
+            newData[0][0]= headField[0].getText();
+            newData[0][1]= headField[1].getText();
+            tableList.add(addToList(headField[0].getText(), headField[1].getText(), headField[2].getText(), "1"));
 
             System.out.println(headField[0].getText());
             System.out.println(headField[1].getText());
+            System.out.println(headField[2].getText());
 
             frame.setContentPane(eventPanel);              
             frame.setVisible(true);
+
+            table();
         }
 
         else if(e.getSource()==submit2) 
         {
-            newData[0][data.length] = eventField[0].getText();
-            newData[1][data.length] = eventField[1].getText();
+            newData[newData.length-1][0] = eventField[0].getText();
+            newData[newData.length-1][1] = eventField[1].getText();
+            newData[newData.length-1][2] = eventField[2].getText();
+            tableList.add(addToList(eventField[0].getText(), eventField[1].getText(), eventField[2].getText(), ""));
 
             frame.setContentPane(eventPanel);               // when the submit button is clicked, the panel changes from head page to event page
             frame.setVisible(true);
@@ -215,13 +252,15 @@ public class schedule implements ActionListener
             System.out.println("Priority: " + priorityBox.getSelectedItem());
 
             for(i=0; i<3; i++)
-        {
-            eventField[i].setText("");   // empties the text boxes once submit is clicked
+            {
+                eventField[i].setText("");   // empties the text boxes once submit is clicked
 
-            // Will not run the code underneath if i is 2 because headField only has 2 indicies
-            if (i == 2) { break; }
-            headField[i].setText("");    
-        }
+                // Will not run the code underneath if i is 2 because headField only has 2 indicies
+                if (i == 2) { break; }
+                headField[i].setText("");    
+            }
+
+            table();
         }
 
         else if(e.getSource() == back)                     
@@ -241,15 +280,22 @@ public class schedule implements ActionListener
             frame.setContentPane(finalPanel);               // when show is clicked, the panel changes from event page to final page
             frame.setVisible(true);
         }
-        
     }
 
     public static void main(String[] args)
     {           
         schedule obj = new schedule();  // obj is the variable
-        obj.table();
-        
-
+        obj.table();       
     }
 
+    private ArrayList addToList(String name, String duration, String time, String priority)
+    {
+        ArrayList temp = new ArrayList<>();
+        temp.add(name);
+        temp.add(duration);
+        temp.add(time);
+        temp.add(priority);
+
+        return temp;
+    }
 }
